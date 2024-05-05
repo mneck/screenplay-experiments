bff709a4acd34611aad10951305d6a09978e8fda
Matt N <mneckelmann@gmail.com>
2024-05-05 20:44:19 +0200

    Add locked out user test

--- src/test/java/saucedemo/Constants.java
+++ src/test/java/saucedemo/Constants.java
@@ -5,5 +5,12 @@ public class Constants {
     public static final String URL = "https://www.saucedemo.com/";
     public static final String STANDARD_USER_USERNAME = "standard_user";
     public static final String PASSWORD = "secret_sauce";
-    public static final String LOGIN_BUTTON_ID = "Login-button";
+    public static final String FIRST_NAME = "Melinda";
+    public static final String LAST_NAME = "Miyamoto";
+    public static final String POSTAL_CODE = "12043";
+
+    public static final String LOCKED_OUT_USER_USERNAME = "locked_out_user";
+
+//    public static final String LOGIN_BUTTON_ID = "Login-button";
+
 }
--- src/test/java/saucedemo/WhenOrderingClothesFromSwagLabs.java
+++ src/test/java/saucedemo/WhenOrderingClothesFromSwagLabs.java
@@ -30,17 +30,68 @@ class WhenOrderingClothesFromSwagLabs {
     @CastMember(name = "Melinda")
     Actor melinda;
 
+    @CastMember(name = "Bill")
+    Actor bill;
+
     @Test
-    @DisplayName("Login with standard username and password")
-    void loginWithUsernameAndPassword() {
+    @DisplayName("Login with standard username and buy a jacket")
+    void loginAndBuyJacket() {
         melinda.attemptsTo(
                 Open.url(URL),
-                Enter.theValue(STANDARD_USER_USERNAME).into("#user-name").thenHit(Keys.RETURN),
-                Enter.theValue(PASSWORD).into("#password").thenHit(Keys.RETURN),
+                Enter.theValue(STANDARD_USER_USERNAME).into("#user-name"),
+                Enter.theValue(PASSWORD).into("#password"),
                 Click.on(Button.withNameOrId("login-button")),
                 Ensure.that(PageElement.withNameOrId("title")).hasTextContent("Products")
         );
 
+        melinda.attemptsTo(
+                Click.on(Button.withNameOrId("add-to-cart-sauce-labs-fleece-jacket")),
+                Ensure.that(PageElement.withNameOrId("shopping-cart-badge")).hasTextContent("1")
+        );
+
+        melinda.attemptsTo(
+                Click.on(PageElement.withNameOrId("shopping-cart-link")),
+                Click.on(Button.withNameOrId("checkout")),
+                Enter.theValue(FIRST_NAME).into("#first-name"),
+                Enter.theValue(LAST_NAME).into("#last-name"),
+                Enter.theValue(POSTAL_CODE).into("#postal-code"),
+                Click.on(Button.withNameOrId("continue")),
+                Click.on(Button.withNameOrId("finish")),
+                Click.on(Button.withNameOrId("back-to-products"))
+
+        );
+
     }
 
+    @Test
+    @DisplayName("Attempt to login with locked out username")
+    void attemptLoginWithLockedOutUsername() {
+        bill.attemptsTo(
+                Open.url(URL),
+                Enter.theValue(LOCKED_OUT_USER_USERNAME).into("#user-name"),
+                Enter.theValue(PASSWORD).into("#password"),
+                Click.on(Button.withNameOrId("login-button")),
+                Ensure.that(Button.withNameOrId("login-button")).isDisplayed()
+        );
+    }
 }
+
+
+
+//
+//    @Test
+//    @DisplayName("Add shirts and jacket to cart")
+//    void AddShirtsAndJacketToCart() {
+//        melinda.attemptsTo(
+//                Open.url("https://www.saucedemo.com/inventory.html"),
+//                Click.on(Button.withNameOrId("add-to-cart-sauce-labs-fleece-jacket")),
+////                Click.on(Button.withNameOrId("add-to-cart-sauce-labs-bolt-t-shirt")),
+////                Click.on(Button.withNameOrId("add-to-cart-test\\.allthethings\\(\\)-t-shirt-\\(red\\)")),
+//                Ensure.that(PageElement.withNameOrId("shopping-cart-badge")).hasTextContent("1"),
+//                Ensure.that(PageElement.withNameOrId("title")).hasTextContent("Products")
+//
+//        );
+
+
+
+
